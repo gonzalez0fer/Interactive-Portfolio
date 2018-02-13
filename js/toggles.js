525d44278fbd14af3fedb73c9915ce1648360a71
Fernando Gonzalez <gonzalez0fer@gmail.com>
2018-02-13 19:14:54 -0400

    algunos avances

--- js/toggles.js
+++ js/toggles.js
@@ -10,18 +10,32 @@ $("#button").click(function() {
 
 $("#button2").on('click', function() {
   var aura = $('.aura');
-
+  var raziel = $('.raziel1');
+  var vela = $('.candle');
+  // register is not visible lets make it visible.
   if(aura.css('opacity') === '0')
   {
     aura.css({
       'opacity': '1',
     });
+    vela.css({
+      'opacity': '0',
+    });
+    raziel.css({
+      'z-index': '209',
+      'filter': 'brightness(160%)',
+    });
   }
   else //We know the opacity is not 0 lets make it 0.
   {
-
     aura.css({
       'opacity': '0',
+    });
+    vela.css({
+      'opacity': '1',
+    });
+    raziel.css({
+      'filter': 'brightness(60%)',
     });
 
   }
