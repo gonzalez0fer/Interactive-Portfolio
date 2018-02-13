4c609b1bd0f80bbbcfab169778274ea60004bb4c
Fernando Gonzalez <gonzalez0fer@gmail.com>
2018-02-13 17:25:25 -0400

    algunos avances

--- js/toggles.js
+++ js/toggles.js
@@ -5,3 +5,24 @@ $("#button").click(function() {
   $('.clouds_two_termina').toggleClass('clouds_two_termina_b');
   /*window.setTimeout(nubes2,2000);*/
 });
+
+
+
+$("#button2").on('click', function() {
+  var aura = $('.aura');
+
+  if(aura.css('opacity') === '0')
+  {
+    aura.css({
+      'opacity': '1',
+    });
+  }
+  else //We know the opacity is not 0 lets make it 0.
+  {
+
+    aura.css({
+      'opacity': '0',
+    });
+
+  }
+});
